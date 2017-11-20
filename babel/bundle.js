af9680f063fcdb4120765f64564e398662b90789
胡海 <huhai@huhaideMacBook-Pro.local>
2017-11-20 17:27:04 +0800

    feat view discard some test demo

---
+++ babel/bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Created by huhai on 17/6/29.
+ */
+// import $ from 'jquery'
+var x = { name: 'huhai', sex: "male" };
+
+function showARG() {
+  console.log(x);
+  debugger;
+}
+
+showARG();
--- css-module-antd/main.js
+++ css-module-antd/main.js
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom';
 import style from './outer.css'
   require('antd/lib/button/style/index.css')
 var Dom = ()=>{
+  debugger
   return <div className={style.hu}> sdfsaf
     <Button type="primary">Primary</Button>
   </div>
